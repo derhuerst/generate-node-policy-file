c5b030bf72d94e303ff5a613d26a900e05851750
Jannis R <mail@jannisr.de>
2019-04-29 18:04:03 +0100

    require hook

---
+++ track.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const {createWriteStream} = require('fs')
+const {addHook} = require('pirates')
+
+const showError = (err) => {
+	console.error(err)
+	process.exit(1)
+}
+
+const destPath = process.env.REQUIRED_FILES
+if (!destPath) showError('Missing env REQUIRED_FILES var.')
+
+const dest = createWriteStream(destPath, {encoding: 'utf-8'})
+dest.on('error', showError)
+
+dest.write('[\n')
+let first = true
+addHook((src, filename) => {
+	if (first) first = false
+	else dest.write(',')
+	dest.write(JSON.stringify(filename) + '\n')
+	return src
+})
+
+process.on('exit', (code) => {
+	dest.end(']\n')
+})
