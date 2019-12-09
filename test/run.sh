5efe24cafde6d09f6c2b678db9cde6b6f38aa656
Jannis R <mail@jannisr.de>
2019-12-09 15:39:04 +0100

    test/run.sh: collect files.json first

--- test/run.sh
+++ test/run.sh
@@ -3,6 +3,7 @@ set -e # fail fast
 cd $(dirname $0)
 set -x # print executed commands
 
+env REQUIRED_FILES=files.json node -r ../track one.js
 ../generate.js --base-dir . files.json >policy.json
 node -v
 node --experimental-policy=policy.json one.js
